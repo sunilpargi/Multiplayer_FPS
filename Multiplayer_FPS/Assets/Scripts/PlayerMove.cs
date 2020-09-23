0995d51e45bbe74e93da9679d5c48991b26d42f4
rahulpargi641 <61396833+rahulpargi641@users.noreply.github.com>
2020-09-23 10:24:29 +0530

    Completed player movement

---
+++ Multiplayer_FPS/Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    public float speed = 10f;
+
+    private Rigidbody rb;
+    void Start()
+    {
+        Camera.main.enabled = false;
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    private void FixedUpdate()
+    {
+        float h_Move = Input.GetAxis("Horizontal");
+        float v_Move = Input.GetAxis("Vertical");
+
+        Vector3 direction = new Vector3(h_Move, 0, v_Move);
+        direction.Normalize();
+
+        rb.velocity = transform.TransformDirection(direction) * speed * Time.deltaTime;
+    }
+}
